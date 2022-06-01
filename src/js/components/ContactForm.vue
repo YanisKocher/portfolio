af06e91e86d9c0a4814dbe09c57be1212eb0fd5c
Yanis <Yanis.Kocher@hotmail.com>
2022-06-01 21:59:32 +0200

    Form validation

--- src/js/components/ContactForm.vue
+++ src/js/components/ContactForm.vue
@@ -1,27 +1,69 @@
 <template>
-    <form action="" class="row g-3">
+    <form action="" class="row g-3" @submit.prevent="submit">
         <div class="form-floating col-12 col-md-6">
-            <input type="text" class="form-control" id="lastname" placeholder="Nom">
+            <input 
+                type="text" 
+                id="lastname"
+                class="form-control" 
+                :class="isValid('lastname')" 
+                placeholder="Nom" 
+                v-model="form.lastname"
+            >
             <label for="lastname">Nom</label>
+            <p class="invalid-feedback mb-0">{{errors.lastname}}</p>
         </div>
         <div class="form-floating col-12 col-md-6">
-            <input type="text" class="form-control" id="firstname" placeholder="Prénom">
+            <input 
+                type="text" 
+                id="firstname" 
+                class="form-control" 
+                :class="isValid('firstname')" 
+                placeholder="Prénom" 
+                v-model="form.firstname"
+            >
             <label for="firstname">Prénom</label>
+            <p class="invalid-feedback mb-0">{{errors.firstname}}</p>
         </div>
 
         <div class="form-floating">
-            <input type="email" class="form-control" id="email" placeholder="contact@yaniskocher.fr">
+            <input 
+                type="email" 
+                id="email" 
+                class="form-control" 
+                :class="isValid('email')" 
+                placeholder="contact@yaniskocher.fr" 
+                v-model="form.email"
+                @input="verificationEmail"
+            >
             <label for="email">Adresse email</label>
+            <p class="invalid-feedback mb-0">{{errors.email}}</p>
         </div>
 
         <div class="form-floating">
-            <input type="text" class="form-control" id="subject" placeholder="Sujet">
+            <input 
+                type="text" 
+                id="subject" 
+                class="form-control" 
+                :class="isValid('subject')" 
+                placeholder="Sujet" 
+                v-model="form.subject"
+                @input="verificationSubject"
+            >
             <label for="subject">Sujet</label>
+            <p class="invalid-feedback mb-0">{{errors.subject}}</p>
         </div>
 
         <div class="form-floating">
-            <textarea class="form-control" placeholder="Ecrivez votre message..." id="message"></textarea>
+            <textarea 
+                id="message" 
+                class="form-control" 
+                :class="isValid('message')" 
+                placeholder="Ecrivez votre message..." 
+                v-model="form.message"
+                @input="verificationMessage"
+            ></textarea>
             <label for="message">Ecrivez votre message...</label>
+            <p class="invalid-feedback mb-0">{{errors.message}}</p>
         </div>
 
         <div class="col-12">
@@ -32,6 +74,59 @@
 
 <script>
 export default {
+    data() {
+        return {
+            form: {
+                lastname: '',
+                firstname: '',
+                email: '',
+                subject: '',
+                message: ''
+            },
+            errors: {
+                lastname: '',
+                firstname: '',
+                email: '',
+                subject: '',
+                message: ''
+            } 
+        }
+    },
 
+    methods: {
+        submit() {
+            for (const key in this.form) {
+                if(this.form[key] === '') {
+                    this.errors[key] = 'Ce champ est obligatoire';
+                }
+            }
+        },
+
+        isValid(label) {
+            if(this.form[label] !== '' && this.errors[label] === '') return 'is-valid';
+            else if(this.errors[label] !== '') return 'is-invalid';
+        },
+
+        verificationEmail() {
+            if(this.form.email !== '' && !this.form.email.match(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+                this.errors.email = 'Votre adresse email n\'est pas valide';
+            } 
+            else this.errors.email = '';
+        },
+
+        verificationSubject() {
+            if(this.form.subject !== '' && this.form.subject.length < 3) {
+                this.errors.subject = 'Votre sujet doit contenir au moins 3 caractères';
+            }
+            else this.errors.subject = '';
+        },
+
+        verificationMessage() {
+            if(this.form.message !== '' && this.form.message.length < 10) {
+                this.errors.message = 'Votre message doit contenir au moins 10 caractères';
+            }
+            else this.errors.message = '';
+        }
+    }
 }
 </script>
--- src/js/components/NavBar.vue
+++ src/js/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav id="navbar" class="navbar col-md-8 col-xl-6">
+    <nav id="navbar" class="navbar col-md-8 col-xl-7 col-xxl-6">
         <ul class="nav nav-pills d-flex align-items-center justify-content-between w-100">
             <li v-for="(label, i) in labels" :key="i" class="nav-item" :class="i == 0 ? 'active' : ''" :data-menuanchor="label.toLowerCase().replace(/[^A-Z0-9]/ig, '')">
                 <a class="nav-link p-0 text-white position-relative" :href="'#' + label.toLowerCase().replace(/[^A-Z0-9]/ig, '')">{{label}}</a>
