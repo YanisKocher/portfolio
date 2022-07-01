22a2c4a3d8dca759912937d98a11d6efc7f94289
Yanis <Yanis.Kocher@hotmail.com>
2022-07-01 22:28:21 +0200

    SEO

--- src/js/App.vue
+++ src/js/App.vue
@@ -8,14 +8,14 @@
       <div class="position-relative h-100">
         <div class="filter position-absolute start-0 top-0 vh-100 w-100"></div>
         <div class="col-6 position-absolute end-0">
-          <img src="../img/kocher_yanis.jpg" alt="" class="vh-100 w-100">
+          <img src="../img/kocher_yanis.jpg" alt="Yanis KOCHER - Développeur web full-stack" class="vh-100 w-100">
         </div>
 
         <div class="container h-100 d-flex align-items-center">
           <h1 class="text-white text-start ps-2 ps-md-0 mb-0">
             <span class="d-block">Je suis</span>
-            Yanis Kocher
-            <span class="d-block text-primary">Développeur full-stack</span>
+            <strong>Yanis Kocher</strong>
+            <span class="d-block text-primary"><strong>Développeur full-stack</strong></span>
           </h1>
         </div>
       </div>
@@ -31,7 +31,7 @@
               <font-awesome-icon :icon="['fa-brands', technologie.icon]" class="d-block mx-auto" />
               <font-awesome-icon :icon="['fa-brands', technologie.icon]" class="position-absolute top-0 start-0" />
             </div>
-            <p class="text-center m-0">{{technologie.label}}</p>
+            <p class="text-center m-0"><strong>{{technologie.label}}</strong></p>
           </div>
         </div>
       </div>
@@ -55,15 +55,15 @@
           <div id="contact_informations" class="col-12 col-md-6 col-xl-4">
           <h2 class="mt-5 mt-md-0">Coordonnées</h2>
           <ul class="p-0">
-            <li class="mb-1"><font-awesome-icon :icon="['fa-solid', 'at']" class="me-2 text-center" />Email : <a href="mailto:contact@yaniskocher.fr">contact@yaniskocher.fr</a></li>
+            <li class="mb-1"><font-awesome-icon :icon="['fa-solid', 'at']" class="me-2 text-center" />Email : <a href="mailto:contact@yaniskocher.fr"><strong>contact@yaniskocher.fr</strong></a></li>
             <li class="mb-1"><font-awesome-icon :icon="['fa-solid', 'phone']" class="me-2 text-center" />Téléphone : <a href="tel:0781999003">07.81.99.90.03</a></li>
             <li class="mb-1">
               <font-awesome-icon :icon="['fa-solid', 'map-marker-alt']" class="me-2 text-center" />
               <p class="d-inline-block m-0">34, rue du Général de Gaulle</p>
               <p class="m-0" style="padding-start: 1.25rem">67640 Lipsheim</p>  
             </li>
-            <li class="mb-1"><font-awesome-icon :icon="['fa-brands', 'github']" class="me-2 text-center" />GitHub : <a href="https://github.com/YanisKocher">YanisKocher</a></li>
-            <li><font-awesome-icon :icon="['fa-brands', 'linkedin']" class="me-2 text-center" />Linkedin : <a href="https://www.linkedin.com/in/yanis-kocher-683492184/">Yanis Kocher</a></li>
+            <li class="mb-1"><font-awesome-icon :icon="['fa-brands', 'github']" class="me-2 text-center" />GitHub : <a href="https://github.com/YanisKocher"><strong>YanisKocher</strong></a></li>
+            <li><font-awesome-icon :icon="['fa-brands', 'linkedin']" class="me-2 text-center" />Linkedin : <a href="https://www.linkedin.com/in/yanis-kocher-683492184/"><strong>Yanis Kocher</strong></a></li>
           </ul>
         </div>
         <div class="col-12 col-md-6 col-xl-8">
--- src/js/ExperienceDescriptions.js
+++ src/js/ExperienceDescriptions.js
@@ -1,13 +1,13 @@
 const ExperiencesDescription = {
-    'DUT' : `<p>La formation MMI est un DUT en informatique sur 2 ans. Cette formation permet l'apprentissage des métiers du web (programmation, graphisme, marketing, etc.).</p>
-    <p>Durant ces 2 années de formation, j'ai pu découvrir les bases du développement web (HTML, CSS, Javascript, etc.) et des outils de développement (VSCode, GitHub, etc.).</p>
-    <p>J'ai pu faire une option développement web pendant le dernier semestre de formation. J'ai également pu faire la deuxième année en alternance.</p>`,
+    'DUT' : `<p>La formation <strong>MMI</strong> est un DUT en informatique sur 2 ans. Cette formation permet l'apprentissage des métiers du web (programmation, graphisme, marketing, etc.).</p>
+    <p>Durant ces 2 années de formation, j'ai pu découvrir les bases du <strong>développement web</strong> (HTML, CSS, Javascript, etc.) et des outils de développement (VSCode, GitHub, etc.).</p>
+    <p>J'ai pu faire une option <strong>développement web</strong> pendant le dernier semestre de formation. J'ai également pu faire la deuxième année en alternance.</p>`,
 
-    'LPDWEB': `<p>Cette licence est un prolongement de la formation MMI. La différence est qu'elle est essentiellement portée sur le développement web.</p>
+    'LPDWEB': `<p>Cette licence est un prolongement de la formation <strong>MMI</strong>. La différence est qu'elle est essentiellement portée sur le <strong>développement web</strong>.</p>
     <p>J'ai pu durant cette année de formation approfondir mes connaissances, et en acquérir de nouvelles.</p>
     <p>Cette année a a été faite en alternance avec la même entreprise que la première année.</p>`,
 
-    'Digicom': `<p>Digicom est une agence de communication spécialisée dans le développement web. Elle est localisée à Strasbourg, mais est également présente dans plusieurs pays.</p>
+    'Digicom': `<p><strong>Digicom</strong> est une agence de communication spécialisée dans le <strong>développement web</strong>. Elle est localisée à <strong>Strasbourg</strong>, mais est également présente dans plusieurs pays.</p>
     <p>Mon rôle dans l'entreprise a été de créer des sites internet via les CMS Wordpress et Prestashop. J'ai également pu faire de la documentation ainsi que de la gestion de projet.</p>`
 }
 
